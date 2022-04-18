b880f9bdab15c2794e2f0ee737acb9cc7622366f
vincentdeveloper <vincentdeveloper101@gmail.com>
2022-04-18 17:24:56 +0300

    password reset module

---
+++ password reset/urls.py
@@ -0,0 +1,32 @@
+from  django.contrib.auth  import views as auth_views
+
+urlpatterns=[ 
+    path('reset_password/',
+        auth_views.PasswordResetView.as_view(template_name="accounts/password_reset.html"),
+        name="reset_password"),
+
+    
+
+    path('reset_password_sent/', 
+        auth_views.PasswordResetDoneView.as_view(template_name="accounts/password_reset_sent.html"), 
+        name="password_reset_done"),
+
+    path('reset/<uidb64>/<token>/',
+     auth_views.PasswordResetConfirmView.as_view(template_name="accounts/password_reset_form.html"), 
+     name="password_reset_confirm"),
+
+    path('reset_password_complete/', 
+        auth_views.PasswordResetCompleteView.as_view(template_name="accounts/password_reset_done.html"), 
+        name="password_reset_complete"),
+]
+
+
+# SETTINGS CONFIGURATIONS
+#SMTP Configuration
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = '*********'
+EMAIL_HOST_PASSWORD = '*********'
